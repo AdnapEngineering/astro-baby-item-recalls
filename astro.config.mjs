8187d11f6c68aba4dfcd70cc4774da02a31e9408
AdnapEngineering <54551478+AdnapEngineering@users.noreply.github.com>
2025-07-28 16:07:46 -0700

    feat(daisyUI): adding Daisy UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://adnapengineering.github.io/astro-baby-item-recalls',
   base: '/astro-baby-item-recalls/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
