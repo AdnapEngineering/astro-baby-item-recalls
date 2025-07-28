23b67d884593087d4b756573522542acf1e5a672
AdnapEngineering <54551478+AdnapEngineering@users.noreply.github.com>
2025-07-28 09:43:01 -0700

    chore(prettier): updating formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://adnapengineering.github.io/astro-baby-item-recalls",
-  base: "/astro-baby-item-recalls/",
+  site: 'https://adnapengineering.github.io/astro-baby-item-recalls',
+  base: '/astro-baby-item-recalls/',
 });
