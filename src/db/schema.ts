9b3158dce1ed0b86de93f7a1b7aa13269d45b6dd
Justin Karcher <adnapengineering@gmail.com>
2026-08-03 15:07:43 -0700

    feat: add database schema for recalls and hazards, update dependencies
    - Introduced new schema for recalls and hazards in src/db/schema.ts using drizzle-orm.
    - Updated package.json to include new dependencies: @libsql/client, drizzle-orm, dotenv, drizzle-kit, and tsx.
    - Enabled script execution for specific versions of esbuild and es5-ext.

---
+++ src/db/schema.ts
@@ -0,0 +1,20 @@
+import { sqliteTable, integer, text, index } from 'drizzle-orm/sqlite-core';
+
+export const recalls = sqliteTable('recalls', {
+recallId: integer('recall_id').primaryKey(),
+title: text('title').notNull(),
+description: text('description'),
+recallDate: text('recall_date').notNull(), //ISO string
+url: text('url'),
+firstSeen: text('first_seen').notNull(),
+lastSeen: text('last_seen').notNull(), 
+});
+
+export const hazards = sqliteTable('hazards', {
+    id: integer('recall_id').notNull().references(()=> recalls.recallId),
+    name: text('name').notNull(),
+},
+table => [index('hazards_name_idx').on(table.name)]
+);
+
+// 
